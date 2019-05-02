2a7564cd5ebd4ed1a8f319b2ff93384729909f6e
valgritim <valerie.baron67@gmail.com>
2019-05-02 15:25:05 +0200

    ajout lien dans dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,9 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('css/app', './assets/css/app.css')
     //.addEntry('page2', './assets/js/page2.js')
-
+    
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
